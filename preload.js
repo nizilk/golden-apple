2e19f2db3b2d97e1eb21e172600fc43633bc5484
王乐言 <66125871+nizilk@users.noreply.github.com>
2026-08-31 23:00:42 +0800

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,104 @@
+const {
+  contextBridge,
+  ipcRenderer
+} = require("electron");
+
+
+contextBridge.exposeInMainWorld(
+  "electronAPI",
+  {
+
+    chooseResourceFolder:
+      () =>
+        ipcRenderer.invoke(
+          "resource:choose"
+        ),
+
+    getXhsDataDirectory:
+      () =>
+        ipcRenderer.invoke(
+          "storage:getXhsDirectory"
+        ),
+
+    readDataFile:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "data:read",
+          relativePath
+        ),
+
+    writeDataFile:
+      (relativePath, data) =>
+        ipcRenderer.invoke(
+          "data:write",
+          relativePath,
+          data
+        ),
+
+    deleteDataFile:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "data:delete",
+          relativePath
+        ),
+
+    createDataDirectory:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "data:mkdir",
+          relativePath
+        ),
+
+    chooseResourceFolder:
+      () =>
+        ipcRenderer.invoke(
+          "resource:choose"
+        ),
+
+    listResourceDirectory:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "resource:list",
+          relativePath
+        ),
+
+    readResourceDataURL:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "resource:readDataURL",
+          relativePath
+        ),
+
+    readDataURL:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "data:readDataURL",
+          relativePath
+        ),
+
+    listDataDirectory:
+      (relativePath) =>
+        ipcRenderer.invoke(
+          "data:list",
+          relativePath
+        ),
+
+    writeDataBinary:
+      async (
+        relativePath,
+        blob
+      ) => {
+
+        const arrayBuffer =
+          await blob.arrayBuffer();
+
+        return await ipcRenderer.invoke(
+          "data:writeBinary",
+          relativePath,
+          arrayBuffer
+        );
+
+      },
+
+  }
+);
